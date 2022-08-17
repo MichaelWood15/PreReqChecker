449855a8533222ee67fc2aaa4556ad50b3dfeadb
MichaelWood15 <111454934+MichaelWood15@users.noreply.github.com>
2022-08-17 15:23:21 -0400

    Update NeedToTake.java

--- PreReqChecker/src/prereqchecker/NeedToTake.java
+++ PreReqChecker/src/prereqchecker/NeedToTake.java
@@ -28,11 +28,102 @@ import java.util.*;
 public class NeedToTake {
     public static void main(String[] args) {
 
-        if ( args.length < 3 ) {
-            StdOut.println("Execute: java NeedToTake <adjacency list INput file> <need to take INput file> <need to take OUTput file>");
-            return;
-        }
+        if ( args.length == 3 ) {
 
+            StdIn.setFile(args[0]);
+            
+            ArrayList<ArrayList<String>> classes = new ArrayList<>();
+            
+            int numofclasses = AdjList.numberofclasses(args[0]);
+            classes = AdjList.Classes(args[0], numofclasses);
+            int numofprereqs = AdjList.numberofprereqs(args[0]);
+
+            for(int i = 0; i < numofclasses; i++){
+                ArrayList<String> prereqs = new ArrayList<>(); 
+                prereqs = AdjList.searchprereqs(args[0], i, numofprereqs, classes);
+                classes.get(i).addAll(prereqs);
+            }
+
+            ArrayList<String> coursestaken = new ArrayList<>();
+            coursestaken = immediatecoursestaken(args[1]);
+
+            ArrayList<String> coursesprevtaken = new ArrayList<>();
+
+            for(int i = 0; i < coursestaken.size(); i++){
+                coursesprevtaken.addAll(ValidPrereq.indirectprereqlist(coursesprevtaken, coursestaken.get(i), classes));
+            }
+            for(int i = 0; i < classes.size(); i++){
+                if (classes.get(i).size() == 1){
+                    coursesprevtaken.add(classes.get(i).get(0));
+                }
+            }
+        
+            coursesprevtaken = removeduplicate(coursesprevtaken);
+            coursesprevtaken.addAll(coursestaken);
+
+            ArrayList<String> needtotake = new ArrayList<>();
+            needtotake = ValidPrereq.indirectprereqlist(needtotake, thecourse(args[1]), classes);
+            needtotake = removeduplicate(needtotake);
+
+            int i = 0;
+            while(i < needtotake.size()) {{
+                if(coursesprevtaken.contains(needtotake.get(i))){
+                    needtotake.remove(needtotake.get(i));
+                    i--;
+                }
+                i++;   
+            }
+
+            
+
+            StdOut.setFile(args[2]);
+            for(int j = 0; j < needtotake.size(); j++){
+                StdOut.println(needtotake.get(j));
+            }
+        }
+    }
 	// WRITE YOUR CODE HERE
+
+    
+    }
+
+    public static int numofcoursestaken(String inputfile)
+    {
+        StdIn.setFile(inputfile);
+        StdIn.readLine();
+        int thenumofcoursestaken = Integer.parseInt(StdIn.readLine());
+        return thenumofcoursestaken;
+    }
+    
+    public static ArrayList<String> immediatecoursestaken(String inputfile)
+    {   
+        ArrayList<String> ict = new ArrayList<>();
+        int num = numofcoursestaken(inputfile);
+        StdIn.setFile(inputfile);
+        StdIn.readLine();
+        StdIn.readLine();
+
+        for(int i = 0; i < num; i++){
+            ict.add(StdIn.readLine());
+        }
+        return ict;
+    }
+
+    public static String thecourse(String inputfile)
+    {
+        StdIn.setFile(inputfile);
+        return StdIn.readLine();
+    }
+
+    public static ArrayList<String> removeduplicate(ArrayList<String> prereqs)
+    {
+        ArrayList<String> duplicatecheck = new ArrayList<>();
+
+        for(int i = 0; i < prereqs.size(); i++){
+            if(!duplicatecheck.contains(prereqs.get(i))){
+                duplicatecheck.add(prereqs.get(i));
+            }
+        }
+        return duplicatecheck;
     }
 }
