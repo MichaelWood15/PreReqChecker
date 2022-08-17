1b55bff04c5a89351ce0e46d06d2b87a6ce688ad
MichaelWood15 <111454934+MichaelWood15@users.noreply.github.com>
2022-08-17 15:24:16 -0400

    Update ValidPrereq.java

--- PreReqChecker/src/prereqchecker/ValidPrereq.java
+++ PreReqChecker/src/prereqchecker/ValidPrereq.java
@@ -1,5 +1,8 @@
 package prereqchecker;
 
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.util.ArrayList;
+
 /**
  * Steps to implement this class main method:
  * 
@@ -23,12 +26,121 @@ package prereqchecker;
  * 1. 1 line, containing either the word "YES" or "NO"
  */
 public class ValidPrereq {
+
     public static void main(String[] args) {
 
-        if ( args.length < 3 ) {
-            StdOut.println("Execute: java -cp bin prereqchecker.ValidPrereq <adjacency list INput file> <valid prereq INput file> <valid prereq OUTput file>");
+        // args0 <adjlist.in> args1 <validprereq.in> args2 <validprereq.out>
+        if ( args.length == 3 ) {
+            
+            String prereq = prerequisite(args[1]);
+            String theclass = classtoprereq(args[1]);
+            
+            StdIn.setFile(args[0]);
+            ArrayList<ArrayList<String>> classes = new ArrayList<>();
+            
+            int numofclasses = AdjList.numberofclasses(args[0]);
+            classes = AdjList.Classes(args[0], numofclasses);
+            int numofprereqs = AdjList.numberofprereqs(args[0]);
+
+            for(int i = 0; i < numofclasses; i++){
+                ArrayList<String> prereqs = new ArrayList<>(); 
+                prereqs = AdjList.searchprereqs(args[0], i, numofprereqs, classes);
+                classes.get(i).addAll(prereqs);
+            }
+            
+            ArrayList<String> indirectprereqs = new ArrayList<>();
+            indirectprereqs = indirectprereqlist(indirectprereqs, prereq, classes);
+            String yesorno;
+            yesorno = yesorno(indirectprereqs, theclass, args[1], args[2]);
+
+            
+            StdOut.setFile(args[2]);
+            StdOut.println(yesorno);
             return;
         }
 	// WRITE YOUR CODE HERE
     }
+
+    public static String prerequisite(String file)
+    {
+        StdIn.setFile(file);
+        StdIn.readLine();
+        String prerequisite = StdIn.readLine();
+        return prerequisite;
+    }
+
+    public static String classtoprereq(String file)
+    {
+        StdIn.setFile(file);
+        String classtoprereq = StdIn.readLine();
+        return classtoprereq;
+
+    }
+
+    public static ArrayList<String> findprereq(ArrayList<ArrayList<String>> thelist, String prereq)
+    
+    {
+        ArrayList<String> thelist2 = new ArrayList<>();
+        for(int i = 0; i < thelist.size(); i++){
+            if(thelist.get(i).get(0).equals(prereq)){
+                thelist2 = thelist.get(i);
+                return thelist2;
+            }
+        }
+        return thelist2;
+    }
+    
+    public static boolean isprereqnotvalid(ArrayList<String> thelist, String theclass)
+    
+    {
+        for(int i = 1; i < thelist.size(); i++){
+            if(thelist.get(i).equals(theclass)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static ArrayList<String> indirectprereqlist (ArrayList<String> indirectprereqs, String prereq, ArrayList<ArrayList<String>> classandprereq)
+    {
+        ArrayList<String> prereqs= new ArrayList<>();
+        prereqs = findprereq(classandprereq, prereq);
+        int num = prereqs.size();
+
+        if(findprereq(classandprereq, prereq).size() != 1){
+            for(int i = 1; i < num; i++){
+                indirectprereqs.add(prereqs.get(i));
+                if(findprereq(classandprereq, prereqs.get(i)).size() != 1){
+                    indirectprereqlist(indirectprereqs, prereqs.get(i), classandprereq);
+                }
+        }
+    }
+        
+        return indirectprereqs;
+    }
+
+    public static String yesorno(ArrayList<String> indirectprereqs, String theclass, String inputfile, String outputfile)
+    {
+        String yesorno = "YES";
+        int num = indirectprereqs.size();
+
+        String prereq = prerequisite(inputfile);
+        String inputclass = classtoprereq(inputfile);
+        
+        if(prereq.equals(inputclass)){
+            StdOut.setFile(outputfile);
+            yesorno = "NO";
+            return yesorno;
+        }
+
+        for(int i = 0; i < num; i++){
+            if(isprereqnotvalid(indirectprereqs, theclass)){
+                yesorno = "NO";
+                return yesorno;
+            }
+            }
+        
+        return yesorno;
+    }
+
 }
